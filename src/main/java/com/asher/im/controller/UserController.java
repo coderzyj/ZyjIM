c3c1f5e264076de93d8df0cca655334790b38604
yongjie.zhang@ucarinc.com <yongjie.zhang@ucarinc.com>
2019-04-03 20:02:23 +0800

    主页面，token鉴权

--- src/main/java/com/asher/im/controller/UserController.java
+++ src/main/java/com/asher/im/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.asher.im.controller;
 
+import ch.qos.logback.classic.Logger;
+import com.alibaba.fastjson.JSON;
 import com.asher.im.base.ApiResponse;
 import com.asher.im.model.User;
 import com.asher.im.service.UserService;
+import com.asher.im.util.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 
 /**
  *
@@ -19,9 +28,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisUtils redisUtils;
 
     @PostMapping("/login")
     @ResponseBody
@@ -30,6 +43,14 @@ public class UserController {
         if(info == null){
             return ApiResponse.ofFailed(null,"用户名/密码错误");
         }
-        return ApiResponse.ofSuccess(info);
+        UUID uuid = UUID.randomUUID();
+        String token = uuid.toString();
+        log.info("生成的token =>{},用户信息=>{}",token, JSON.toJSONString(info));
+        //如果验证通过则生成令牌并存入redis，默认过期时间30分钟
+        redisUtils.set(token,JSON.toJSONString(info),1800);
+        Map<String,String> map = new HashMap<>(16);
+        map.put("token",token);
+        map.put("info",JSON.toJSONString(info));
+        return ApiResponse.ofSuccess(map);
     }
 }
--- src/main/java/com/asher/im/socket/IMSocketHandler.java
+++ src/main/java/com/asher/im/socket/IMSocketHandler.java
@@ -1,10 +1,13 @@
 package com.asher.im.socket;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -15,7 +18,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
  * @Version : v1.0
  * @description
  **/
-@ServerEndpoint("/websocket")
+@ServerEndpoint("/websocket/{username}")
 @Component
 public class IMSocketHandler{
     private static CopyOnWriteArraySet<IMSocketHandler> websocketSet = new CopyOnWriteArraySet<>();
@@ -25,22 +28,42 @@ public class IMSocketHandler{
      */
     private Session session;
 
-
+    /**
+     * 当前连接用户名
+     */
+    private String username;
     /**
      * websocket连接成功时调用的方法
      * @param session
      */
     @OnOpen
-    public void onOpen(Session session){
+    public void onOpen(Session session, @PathParam("username") String username){
         this.session = session;
         websocketSet.add(this);
-
+        this.username = username;
         System.out.println("当前在线人数:"+websocketSet.size()+"人");
-        this.session.getAsyncRemote().sendText("连接成功");
+        this.session.getAsyncRemote().sendText(this.username+",你已连接成功");
     }
 
+    /**
+     * websocket关闭时执行的方法
+     */
     @OnClose
     public void onClose(){
+        //从保存会话的set中移除
+        websocketSet.remove(this);
+        System.out.println("有一个用户断开连接，当前在线人数："+websocketSet.size()+"人");
+    }
+
+
+    /**
+     * 处理客户端传过来的消息的方法
+     * @param session
+     * @param message
+     * @param username
+     */
+    @OnMessage
+    public void onMessage(Session session,String message,@PathParam("username") String username){
 
     }
 }
